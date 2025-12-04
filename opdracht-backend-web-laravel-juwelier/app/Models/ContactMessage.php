60471193b404090ef07e6bf4e39482d8e94aede4
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-12-04 15:57:17 +0100

    bewerken van model ContactMessage

--- opdracht-backend-web-laravel-juwelier/app/Models/ContactMessage.php
+++ opdracht-backend-web-laravel-juwelier/app/Models/ContactMessage.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ContactMessage extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'subject',
+        'message',
+        'is_read',
+    ];
+
+    protected function casts(): array{
+        return[
+            'is_read'=>'boolean',
+        ];
+    }
 }
