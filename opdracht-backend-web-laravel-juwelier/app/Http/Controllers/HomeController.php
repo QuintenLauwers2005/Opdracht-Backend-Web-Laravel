91fc67ca041ecc7a48d878fc094c7e258b073767
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-12-05 16:32:30 +0100

    Homecontroller aangepast

--- opdracht-backend-web-laravel-juwelier/app/Http/Controllers/HomeController.php
+++ opdracht-backend-web-laravel-juwelier/app/Http/Controllers/HomeController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\NewsItem;
+use App\Models\JewelryProduct;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $latestNews = NewsItem::with('user')
+            ->orderBy('publication_date', 'desc')
+            ->take(3)
+            ->get();
+
+        $featuredProducts = JewelryProduct::with('category')
+            ->inRandomOrder()
+            ->take(4)
+            ->get();
+
+        return view('home', compact('latestNews', 'featuredProducts'));
+    }
 }
