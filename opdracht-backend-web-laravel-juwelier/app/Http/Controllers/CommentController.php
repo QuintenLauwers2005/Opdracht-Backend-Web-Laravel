4792ea27ead844a4e3af9ed25d493d1957efe9da
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-12-05 17:18:31 +0100

    Uitwerken van CommentController

--- opdracht-backend-web-laravel-juwelier/app/Http/Controllers/CommentController.php
+++ opdracht-backend-web-laravel-juwelier/app/Http/Controllers/CommentController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\NewsItem;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, NewsItem $newsItem){
+
+        $validated = $request->validate([
+            'content' => 'required|string|min:3|max:1000',
+        ], [
+            'content.required' => 'Reactie is verplicht',
+            'content.min' => 'Reactie moet minimaal 3 karakters bevatten',
+        ]);
+
+        $newsItem->comments()->create([
+            'user_id' => auth()->id(),
+            'content' => $validated['content'],
+        ]);
+
+        return redirect()->route('news.show', $newsItem)
+            ->with('success', 'Reactie geplaatst!');
+    }
 }
