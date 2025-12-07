79a77c7bc78be6a9f104a07480ff5d319b4dec15
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-12-07 19:01:58 +0100

    AdminContactController uitgewerkt, nu is er een overzicht van contactberichten, onderscheid tussen gelezen en ongelezen, een mogelijkheid om direct te antwoorden met de mailto en het bericht verwijderen

--- opdracht-backend-web-laravel-juwelier/app/Http/Controllers/Admin/AdminContactController.php
+++ opdracht-backend-web-laravel-juwelier/app/Http/Controllers/Admin/AdminContactController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\ContactMessage;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AdminContactController extends Controller
 {
-    //
+    public function index()
+    {
+        $messages = ContactMessage::orderBy('created_at', 'desc')->paginate(20);
+        return view('admin.contact.index', compact('messages'));
+    }
+
+    public function show(ContactMessage $message)
+    {
+        if (!$message->is_read) {
+            $message->update(['is_read' => true]);
+        }
+        return view('admin.contact.show', compact('message'));
+    }
+
+    public function destroy(ContactMessage $message)
+    {
+        $message->delete();
+        return redirect()->route('admin.contact.index')
+            ->with('success', 'bericht verwijderd');
+    }
+
 }
