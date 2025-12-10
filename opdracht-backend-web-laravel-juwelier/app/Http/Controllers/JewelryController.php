f9cd9ef8bcd1ae207947b95a74de4ba0720e9d7e
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-12-10 21:27:51 +0100

    de algemene controller voor jewelry uitgewerkt

--- opdracht-backend-web-laravel-juwelier/app/Http/Controllers/JewelryController.php
+++ opdracht-backend-web-laravel-juwelier/app/Http/Controllers/JewelryController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JewelryProduct;
+use App\Models\JewelryCategory;
 use Illuminate\Http\Request;
 
 class JewelryController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = JewelryProduct::with('category');
+
+        if ($request->has('category') && $request->category != '') {
+            $query->where('jewelry_category_id', $request->category);
+        }
+
+        $products = $query->paginate(12);
+        $categories = JewelryCategory::all();
+
+        return view('jewelry.index', compact('products', 'categories'));
+    }
+
+    public function show(JewelryProduct $product)
+    {
+        $product->load('category');
+        $relatedProducts = JewelryProduct::where('jewelry_category_id', $product->jewelry_category_id)
+            ->where('id', '!=', $product->id)
+            ->take(4)
+            ->get();
+
+        return view('jewelry.show', compact('product', 'relatedProducts'));
+    }
 }
