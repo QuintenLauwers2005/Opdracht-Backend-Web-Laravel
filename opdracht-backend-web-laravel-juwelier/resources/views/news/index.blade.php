25b38f72096d64ad6f635f01e2dc2015c66eaf5c
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-12-24 17:57:16 +0100

    detail in orde gebracht index voor view news

--- opdracht-backend-web-laravel-juwelier/resources/views/news/index.blade.php
+++ opdracht-backend-web-laravel-juwelier/resources/views/news/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Nieuws - Juwelier Lauwers')
+@section('title', 'Nieuws - Juwelier Hendrickx')
 
 @section('content')
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
